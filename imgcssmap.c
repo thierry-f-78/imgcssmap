6d597fc7e46db0cac33312ce8474661891d9f55f
thierry <thierry>
2011-12-26 19:04:35 +0100

    Add licence

--- imgcssmap.c
+++ imgcssmap.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2011-2012 Thierry FOURNIER
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License.
+ *
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
